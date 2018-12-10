0cc22bf47db762103ef18b335011f8400e71db4c
paul <zerek81@gmail.com>
2018-12-10 17:45:19 +0100

    steintest

---
+++ Assets/Paul/Scripts/SpawnStone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnStone : MonoBehaviour
+{
+   public GameObject RollingStone;
+    // IEnumerator Start() {
+        
+    //     yield return new WaitForSeconds(2f);
+    //     Debug.Log("waited for 2 sec");
+    // }
+    IEnumerator Spawn()
+    {
+        Instantiate(RollingStone, transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(2f);
+        Debug.Log("waited for 2 sec");
+    }
+
+   
+}
