f92c2afbf79ede2ac096e5d7624b8a3b6657989f
Hadi <simiyaworld@gmail.com>
2018-12-12 23:01:41 +0100

    setup in laptop

---
+++ Assets/Final/_Scripts/ParentMechanics.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentMechanics : MonoBehaviour
+{
+    public GameObject myPlayer;
+
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject == myPlayer)
+        {
+            myPlayer.transform.parent = transform;
+        }
+
+
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject == myPlayer)
+        {
+            myPlayer.transform.parent = null;
+        }
+    }
+
+
+
+}
