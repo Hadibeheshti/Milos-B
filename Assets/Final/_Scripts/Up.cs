872cdadf34e4eb769633282792208f43fb0b7694
Hadi <simiyaworld@gmail.com>
2019-02-10 19:19:48 +0100

    10-02-2019 Abend

---
+++ Assets/Final/_Scripts/Up.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Up : MonoBehaviour
+{
+    public GameObject UpperModel;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {   
+       
+
+    }
+
+    //Down
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+            anim.SetBool("Down", true);
+
+
+        }
+
+
+    }
+
+    //Up
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            anim.SetBool("Down", false);
+
+        }
+
+
+    }
+
+
+}
